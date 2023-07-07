c1cc0eae8dc11cdf63885c6a9f3d4d34c2b60bff
Philip Napoli <dubomb1011@gmail.com>
2023-07-07 19:01:46 -0400

    Cleaned up play function code

--- src/game.cpp
+++ src/game.cpp
@@ -18,49 +18,32 @@ bool Game::init() {
 }
 
 void Game::play() {
-    auto window = sf::RenderWindow{ { 600u, 480u }, "CMake SFML Project" };
+    auto window = sf::RenderWindow{ { 600u, 480u }, "snapshot" };
     window.setVerticalSyncEnabled(true);
-    bool grabbed = false;
 
     FontLibrary& fontLibrary = FontLibrary::getInstance();
     TextureLibrary& textureLibrary = TextureLibrary::getInstance();
 
-    sf::Text text("Hello!", fontLibrary.getFont("VT"), 50U);
-    text.setPosition({ 100, 100 });
-    text.setFillColor(sf::Color::White);
-
-    sf::Sprite sprite(textureLibrary.getTexture("square"));
-    sprite.setPosition({ 300, 300 });
-    sprite.setScale({ 3, 3 });
-    sprite.setOrigin({ 8.0f, 8.0f });
-
     sf::Clock time;
 
-    while (window.isOpen())
+    bool quit = false;
+
+    while (!quit)
     {
         float delta = time.restart().asSeconds();
-
-        window.requestFocus();
 
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
             if (event.type == sf::Event::Closed)
             {
-                window.close();
-            }
-
-            if (event.type == sf::Event::MouseButtonPressed) {
-                window.setMouseCursorGrabbed(!grabbed);
-                grabbed = !grabbed;
+                quit = true;
             }
         }
 
         window.clear();
 
-        window.draw(text);
-        sprite.rotate(50.0f * delta);
-        window.draw(sprite);
-
         window.display();
     }
+
+    window.close();
 }
