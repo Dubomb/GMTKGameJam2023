eef2116ceb302ec88443f4f45cccca3f66f25e17
Philip Napoli <dubomb1011@gmail.com>
2023-07-07 22:20:02 -0400

    Added window with mouse follow and position lock

--- src/game.cpp
+++ src/game.cpp
@@ -30,11 +30,14 @@ void Game::play() {
 
     sf::Text playText("Play", fontLibrary.getFont("VT"), 64);
     playText.setFillColor(sf::Color::White);
-    playText.setOrigin(playText.getGlobalBounds().width / 2.0f, playText.getGlobalBounds().height / 2.0f);
+    playText.setOrigin(playText.getGlobalBounds().width / 2.0f,
+            playText.getGlobalBounds().height / 2.0f);
     playText.setPosition(float(windowSize.x * 0.5f), float(windowSize.y * 0.5f));
+
     sf::Text creditsText("Credits", fontLibrary.getFont("VT"), 64);
     creditsText.setFillColor(sf::Color::White);
-    creditsText.setOrigin(creditsText.getGlobalBounds().width / 2.0f, creditsText.getGlobalBounds().height / 2.0f);
+    creditsText.setOrigin(creditsText.getGlobalBounds().width / 2.0f, 
+            creditsText.getGlobalBounds().height / 2.0f);
     creditsText.setPosition(float(windowSize.x * 0.5f), float(windowSize.y * 0.75f));
 
     sf::Clock time;
@@ -57,16 +60,14 @@ void Game::play() {
                 sf::Vector2i mousePos = getWindowMousePosition(window);
 
                 if (playText.getGlobalBounds().contains(sf::Vector2f(mousePos))) {
-                    sf::Vector2i windowPos = window.getPosition();
-                    window.setPosition({ (int)getDesktopSize().x + 10, (int)getDesktopSize().y + 10 });
+                    window.setVisible(false);
                     gameLoop();
-                    window.setPosition(windowPos);
+                    window.setVisible(true);
                 }
                 else if (creditsText.getGlobalBounds().contains(sf::Vector2f(mousePos))) {
-                    sf::Vector2i windowPos = window.getPosition();
-                    window.setPosition({ (int)getDesktopSize().x + 10, (int)getDesktopSize().y + 10 });
+                    window.setVisible(false);
                     openCredits();
-                    window.setPosition(windowPos);
+                    window.setVisible(true);
                 }
             }
         }
@@ -83,19 +84,58 @@ void Game::play() {
 }
 
 void Game::gameLoop() {
+    sf::Vector2u fixedSize(getFixedDesktop());
+
+    FontLibrary& fontLibrary = FontLibrary::getInstance();
+    TextureLibrary& textureLibrary = TextureLibrary::getInstance();
+
+    const int windowScale = 6;
+
+    sf::Vector2u windowSize = sf::Vector2u(fixedSize.y / windowScale, fixedSize.x / windowScale);
+
+    sf::RenderWindow window(sf::VideoMode(windowSize.x, windowSize.y), "", sf::Style::None);
+    window.setVerticalSyncEnabled(true);
+    window.setVisible(true);
+    window.setMouseCursorVisible(false);
+
     std::cout << "Begin the game!\n";
 
-    _sleep(1000);
+    bool exit = false;
+    bool lockedMouse = true;
+
+    sf::Clock clock;
+
+    while (!exit) {
+        for (auto event = sf::Event{}; window.pollEvent(event);) {
+            if (event.type == sf::Event::KeyPressed &&
+                event.key.scancode == sf::Keyboard::Scan::Escape) {
+                exit = true;
+            }
+        }
+
+        window.clear(sf::Color(255, 255, 255, 255));
+
+        if (lockedMouse) {
+            window.setPosition(sf::Vector2i(fixedSize.x / 2 - windowSize.x / 2, fixedSize.y / 2 - windowSize.y / 2));
+        }
+        else {
+            window.setPosition(sf::Vector2i(getMousePosition().x - windowSize.x / 2, getMousePosition().y - windowSize.y / 2));
+        }
+
+        window.display();
+    }
 }
 
 void Game::openCredits() {
     auto window = sf::RenderWindow{ { 600u, 480u }, "Credits", sf::Style::Titlebar };
+    window.setVerticalSyncEnabled(true);
 
     bool exit = false;
 
     while (!exit) {
         for (auto event = sf::Event{}; window.pollEvent(event);) {
-            if (event.type == sf::Event::KeyPressed && event.key.scancode == sf::Keyboard::Scan::Escape) {
+            if (event.type == sf::Event::KeyPressed &&
+                        event.key.scancode == sf::Keyboard::Scan::Escape) {
                 exit = true;
             }
         }
